6a058513abed044e700ade0a34df79968b35e839
Souporno Ghosh <souporno2000@gmail.com>
2021-01-08 22:16:22 +0530

    Updates

---
+++ precache-manifest.9551428e50833c106cec701163d32a4b.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e473f8f4254f3197b9d9ce4431a0a25",
+    "url": "/TheAnonymousMan/theanonymousman.github.io/index.html"
+  },
+  {
+    "revision": "6a7993cd4040b9808699",
+    "url": "/TheAnonymousMan/theanonymousman.github.io/static/css/2.ac4895e7.chunk.css"
+  },
+  {
+    "revision": "5327bd1185e16a82d01c",
+    "url": "/TheAnonymousMan/theanonymousman.github.io/static/css/main.4951e0a3.chunk.css"
+  },
+  {
+    "revision": "6a7993cd4040b9808699",
+    "url": "/TheAnonymousMan/theanonymousman.github.io/static/js/2.8ca5e1cb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/TheAnonymousMan/theanonymousman.github.io/static/js/2.8ca5e1cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5327bd1185e16a82d01c",
+    "url": "/TheAnonymousMan/theanonymousman.github.io/static/js/main.85997c8c.chunk.js"
+  },
+  {
+    "revision": "dd45076f5fe6dd3f91a5",
+    "url": "/TheAnonymousMan/theanonymousman.github.io/static/js/runtime-main.61eb9188.js"
+  },
+  {
+    "revision": "6ae134f4a911e191374c733af0792b00",
+    "url": "/TheAnonymousMan/theanonymousman.github.io/static/media/Back Home.6ae134f4.png"
+  },
+  {
+    "revision": "ce6957ef15fbae6d10f3f60918c9e96d",
+    "url": "/TheAnonymousMan/theanonymousman.github.io/static/media/Box Shooter.ce6957ef.png"
+  },
+  {
+    "revision": "9003593259bcf6797813644f4c6b227c",
+    "url": "/TheAnonymousMan/theanonymousman.github.io/static/media/Everything Is Fine.90035932.png"
+  },
+  {
+    "revision": "0e1e39212576ef0ca0faf5fe15b63c8b",
+    "url": "/TheAnonymousMan/theanonymousman.github.io/static/media/Keep Clive Alive.0e1e3921.png"
+  },
+  {
+    "revision": "7820de26a3e98e2f1e69ff4c3eaed766",
+    "url": "/TheAnonymousMan/theanonymousman.github.io/static/media/Le Moi drawing Le Moi.7820de26.png"
+  },
+  {
+    "revision": "abd2a343bb5c395535e85ee343b07a19",
+    "url": "/TheAnonymousMan/theanonymousman.github.io/static/media/Roller Madness.abd2a343.png"
+  },
+  {
+    "revision": "c501a0714665b13fed0fe021f990da8f",
+    "url": "/TheAnonymousMan/theanonymousman.github.io/static/media/Solar System Simulation.c501a071.png"
+  },
+  {
+    "revision": "2f12242375edd68e9013ecfb59c672e9",
+    "url": "/TheAnonymousMan/theanonymousman.github.io/static/media/fa-brands-400.2f122423.svg"
+  },
+  {
+    "revision": "8300bd7f30e0a313c1d772b49d96cb8e",
+    "url": "/TheAnonymousMan/theanonymousman.github.io/static/media/fa-brands-400.8300bd7f.ttf"
+  },
+  {
+    "revision": "ad527cc5ec23d6da66e8a1d6772ea6d3",
+    "url": "/TheAnonymousMan/theanonymousman.github.io/static/media/fa-brands-400.ad527cc5.woff"
+  },
+  {
+    "revision": "e2ca6541bff3a3e9f4799ee327b28c58",
+    "url": "/TheAnonymousMan/theanonymousman.github.io/static/media/fa-brands-400.e2ca6541.eot"
+  },
+  {
+    "revision": "f075c50f89795e4cdb4d45b51f1a6800",
+    "url": "/TheAnonymousMan/theanonymousman.github.io/static/media/fa-brands-400.f075c50f.woff2"
+  },
+  {
+    "revision": "3602b7e8b2cb1462b0bef9738757ef8a",
+    "url": "/TheAnonymousMan/theanonymousman.github.io/static/media/fa-regular-400.3602b7e8.svg"
+  },
+  {
+    "revision": "3c6879c4f342203d099bdd66dce6d396",
+    "url": "/TheAnonymousMan/theanonymousman.github.io/static/media/fa-regular-400.3c6879c4.woff"
+  },
+  {
+    "revision": "49f00693b0e5d45097832ef5ea1bc541",
+    "url": "/TheAnonymousMan/theanonymousman.github.io/static/media/fa-regular-400.49f00693.ttf"
+  },
+  {
+    "revision": "4a74738e7728e93c4394b8604081da62",
+    "url": "/TheAnonymousMan/theanonymousman.github.io/static/media/fa-regular-400.4a74738e.woff2"
+  },
+  {
+    "revision": "b01516c1808be557667befec76cd6318",
+    "url": "/TheAnonymousMan/theanonymousman.github.io/static/media/fa-regular-400.b01516c1.eot"
+  },
+  {
+    "revision": "205f07b3883c484f27f40d21a92950d4",
+    "url": "/TheAnonymousMan/theanonymousman.github.io/static/media/fa-solid-900.205f07b3.ttf"
+  },
+  {
+    "revision": "4451e1d86df7491dd874f2c41eee1053",
+    "url": "/TheAnonymousMan/theanonymousman.github.io/static/media/fa-solid-900.4451e1d8.woff"
+  },
+  {
+    "revision": "664de3932dd6291b4b8a8c0ddbcb4c61",
+    "url": "/TheAnonymousMan/theanonymousman.github.io/static/media/fa-solid-900.664de393.svg"
+  },
+  {
+    "revision": "8ac3167427b1d5d2967646bd8f7a0587",
+    "url": "/TheAnonymousMan/theanonymousman.github.io/static/media/fa-solid-900.8ac31674.eot"
+  },
+  {
+    "revision": "8e1ed89b6ccb8ce41faf5cb672677105",
+    "url": "/TheAnonymousMan/theanonymousman.github.io/static/media/fa-solid-900.8e1ed89b.woff2"
+  }
+]);
